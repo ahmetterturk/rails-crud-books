6b9c9838cf91c099b6e2a345e81957949a894499
ahmetterturk <ahm.ert@hotmail.com>
2021-11-04 21:47:08 +1100

    complete crud operations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,49 @@
 class BooksController < ApplicationController
     def index
-        
+        @books = Book.all
+    end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        if @book.save 
+            redirect_to books_path
+        else
+            redirect_to books_new_path
+            flash.now[:alert] = 'Error while creating book!'
+        end 
+    end
+
+    def edit 
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+            redirect_to book_path(@book)
+        else
+            redirect_to edit_book_path(@book)
+            flash.now[:alert] = 'Error while editing book!'
+        end
+    end
+
+    def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        redirect_to books_path
+    end
+
+    private
+    def book_params
+        params.require(:book).permit(:name, :author, :description, :year)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   
   get '/', to: 'books#index'
-  get '/books', to: 'books#index'
-  get '/books/new', to: 'books#new' 
-  post '/books', to: 'books#create'
-  get '/books/:id', to: 'books#show'
-  put '/books/:id', to: 'books#update'
-  delete '/books/:id', to: 'books#destroy'
+  # get '/books', to: 'books#index'
+  # get '/books/new', to: 'books#new' 
+  # post '/books', to: 'books#create'
+  # get '/books/:id', to: 'books#show', as: 'book'
+  # get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  # put '/books/:id', to: 'books#update'
+  # delete '/books/:id', to: 'books#destroy'
+
+  resources :books
 end
